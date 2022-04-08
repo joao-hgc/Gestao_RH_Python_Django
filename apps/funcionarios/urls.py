3b20da5ac85b5861e0ca3cace01bae3ba4d603a8
João Henrique Garcia Conte <joao_hgc@hotmail.com>
2022-04-08 17:14:11 -0300

    CRUD funcionarios update

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import FuncionariosList
+from .views import FuncionariosList, FuncionarioEdit
 
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('editar/<int:pk>', FuncionarioEdit.as_view(), name='update_funcionario'),
 ]
