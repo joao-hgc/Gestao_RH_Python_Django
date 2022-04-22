703cde988a2a81da7a8e272a1a4fa8e2bad2c94c
João Henrique Garcia Conte <joao_hgc@hotmail.com>
2022-04-22 14:01:37 -0300

    CRUD documentos parte final

--- apps/documentos/urls.py
+++ apps/documentos/urls.py
@@ -3,6 +3,6 @@ from .views import DocumentoCreate
 
 
 urlpatterns = [
-    path('novo', DocumentoCreate.as_view(), name='create_documento'),
+    path('novo/<int:funcionario_id>/', DocumentoCreate.as_view(), name='create_documento'),
     #path('deletar/<int:pk>', DocumentoDelete.as_view(), name='delete_documento'),
 ]
