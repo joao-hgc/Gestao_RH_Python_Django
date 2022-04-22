5414a7674e3aef46a18e2526ba47721d3d6f52bb
João Henrique Garcia Conte <joao_hgc@hotmail.com>
2022-04-22 12:44:03 -0300

    CRUD de documentos

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from apps.funcionarios.models import Funcionario
 
 
-class Documentos(models.Model):
+class Documento(models.Model):
     descricao = models.CharField(max_length=100)
     pertence = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
+    arquivo = models.FileField(upload_to='documentos')
 
     def __str__(self):
         return self.descricao
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -91,8 +91,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 LOGIN_REDIRECT_URL = 'home'
 
