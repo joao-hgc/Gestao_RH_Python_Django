a5765132a98ead68327fb381934a287549961bd3
João Henrique Garcia Conte <joao_hgc@hotmail.com>
2022-04-25 18:04:16 -0300

    CRUD banco de horas delete

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 from .views import (
     HoraExtraList,
     HoraExtraEdit,
-    # FuncionarioDelete,
-    # FuncionarioNovo
+    HoraExtraDelete
 )
 
 urlpatterns = [
     path('', HoraExtraList.as_view(), name='list_hora_extra'),
    # path('novo', FuncionarioNovo.as_view(), name='create_funcionario'),
     path('editar/<int:pk>', HoraExtraEdit.as_view(), name='update_hora_extra'),
-   # path('delete/<int:pk>', FuncionarioDelete.as_view(), name='delete_funcionario'),
+    path('delete/<int:pk>', HoraExtraDelete.as_view(), name='delete_hora_extra'),
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from .models import RegistroHoraExtra
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
 
@@ -12,3 +13,8 @@ class HoraExtraList(ListView):
 class HoraExtraEdit(UpdateView):
     model = RegistroHoraExtra
     fields = ['motivo', 'funcionario', 'horas']
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
